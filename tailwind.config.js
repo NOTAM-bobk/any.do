f3540e27061a45cacbeba1e6b73ebe894ca54d86
NOTAM <sawyerbobk563@gmail.com>
2026-05-11 19:30:24 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
+/* Adding a tiny bit of global CSS to ensure the body covers the full screen */
+body, html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #FFE600;
+}
